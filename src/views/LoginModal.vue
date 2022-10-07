d13f6d63041998df88d14b0fa31dee8c97a406d5
Naveed Hassan <08es34@gmail.com>
2022-10-07 21:21:20 +0500

    login modal updated

--- src/views/LoginModal.vue
+++ src/views/LoginModal.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import UModal from '@/U/components/UModal.vue'
 import { useLoginStore } from 'src/stores/login.store'
+import { useNotify } from 'src/U/composables/Notifiy'
+import { Validator } from 'src/Vee/validator'
 import GoogleIcon from '../material-design-icons/Google.vue'
 import { useAuthStore } from '../stores/auth.store'
 import UButton from '../U/components/UButton.vue'
@@ -10,15 +12,20 @@ import { minLengthRule } from '../Vee/rules/minLength.rule'
 import { requiredRule } from '../Vee/rules/required.rule'
 import { useValidator } from '../Vee/useValidator'
 
+const notify = useNotify()
 const auth = useAuthStore()
 const store = useLoginStore()
-const v = useValidator(auth.form, v => {
+const v = useValidator(auth.form, (v: Validator) => {
     v.addRule(requiredRule('email'))
     v.addRule(emailRule('email'))
     v.addRule(requiredRule('password'))
     v.addRule(minLengthRule('password', 6))
 
-    v.addCustomRule('name', 'Name is required', v => !store.isRegisterPage || auth.form.name.length)
+    v.addRule(requiredRule('name'), v => store.isRegisterPage)
+
+    v.addRule(requiredRule('otp'), v => store.isForgotPage && store.showOtp)
+    v.addRule(requiredRule('newPassword'), v => store.isForgotPage && store.showOtp)
+    v.addRule(minLengthRule('newPassword', 6), v => store.isForgotPage && store.showOtp)
 })
 
 function close() {
@@ -33,6 +40,17 @@ function submit() {
         auth.login().then(close)
     } else if (store.isRegisterPage) {
         auth.register().then(close)
+    } else if (store.isForgotPage && !store.showOtp) {
+        auth.sendForgotReq().then(data => {
+            store.showOtp = true
+            notify.success('Email sent', 'Please check your inbox')
+        })
+    } else if (store.isForgotPage && store.showOtp) {
+        auth.resetPassword().then(data => {
+            store.showOtp = false
+            notify.success('Success', 'Your password has been reset')
+            store.gotoLogin()
+        })
     }
 }
 
@@ -58,9 +76,23 @@ function submit() {
                     :errors="v.errors.password"
                     v-if="!store.isForgotPage"
                 />
+                <UInput v-if="store.isForgotPage && store.showOtp"
+                        type="text"
+                        v-model="auth.form.otp"
+                        label="OTP"
+                        class="mb-4"
+                        :errors="v.errors.otp"
+                />
+                <UInput v-if="store.isForgotPage && store.showOtp"
+                        type="password"
+                        v-model="auth.form.newPassword"
+                        label="New Password"
+                        class="mb-4"
+                        :errors="v.errors.newPassword"
+                />
 
                 <div class="d-flex align-items-center justify-content-between">
-                    <UButton :loading="auth.loginReq.loading || auth.signupReq.loading">
+                    <UButton :loading="auth.loginReq.loading || auth.signupReq.loading || auth.forgotReq.loading">
                         {{ store.isLoginPage ? 'Login' : 'Submit' }}
                     </UButton>
 
