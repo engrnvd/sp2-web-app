6f1e4b5db49744bb87a8c7d4493a61d2a5e185f4
Naveed Hassan <08es34@gmail.com>
2022-10-07 18:37:08 +0500

    register + forgot views

--- src/views/LoginModal.vue
+++ src/views/LoginModal.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import UModal from '@/U/components/UModal.vue'
-import { FetchRequest } from '../helpers/fetch-request'
+import { useLoginStore } from 'src/stores/login.store'
 import GoogleIcon from '../material-design-icons/Google.vue'
 import { useAuthStore } from '../stores/auth.store'
 import UButton from '../U/components/UButton.vue'
@@ -11,20 +11,29 @@ import { requiredRule } from '../Vee/rules/required.rule'
 import { useValidator } from '../Vee/useValidator'
 
 const auth = useAuthStore()
+const store = useLoginStore()
 const v = useValidator(auth.form, v => {
     v.addRule(requiredRule('email'))
     v.addRule(emailRule('email'))
     v.addRule(requiredRule('password'))
     v.addRule(minLengthRule('password', 6))
+
+    v.addCustomRule('name', 'Name is required', v => !store.isRegisterPage || auth.form.name.length)
 })
+
+function close() {
+    auth.modals.login = false
+}
 
 function submit() {
     v.validate()
     if (v.hasErrors) return
 
-    auth.login().then(data => {
-        auth.modals.login = false
-    })
+    if (store.isLoginPage) {
+        auth.login().then(close)
+    } else if (store.isRegisterPage) {
+        auth.register().then(close)
+    }
 }
 
 </script>
@@ -32,30 +41,48 @@ function submit() {
 <template>
     <UModal
         v-model="auth.modals.login"
-        title="Log in"
+        :title="store.page"
         size="sm"
         no-footer
         body-class="p-0">
         <div style="padding: 3em">
             <form action="" @submit.prevent="submit">
+                <UInput v-if="store.isRegisterPage" type="text" v-model="auth.form.name" label="Name" class="mb-4"
+                        :errors="v.errors.name"/>
                 <UInput type="text" v-model="auth.form.email" label="Email" class="mb-4" :errors="v.errors.email"/>
-                <UInput type="password" v-model="auth.form.password" label="Password" class="mb-4"
-                        :errors="v.errors.password"/>
+                <UInput
+                    type="password"
+                    v-model="auth.form.password"
+                    label="Password"
+                    class="mb-4"
+                    :errors="v.errors.password"
+                    v-if="!store.isForgotPage"
+                />
 
                 <div class="d-flex align-items-center justify-content-between">
-                    <UButton :loading="auth.loginReq.loading">Login</UButton>
-                    <a href="" class="text-base" @click.prevent="">Forgot password?</a>
+                    <UButton :loading="auth.loginReq.loading || auth.signupReq.loading">
+                        {{ store.isLoginPage ? 'Login' : 'Submit' }}
+                    </UButton>
+
+                    <a href=""
+                       class="text-base"
+                       @click.prevent="() => store.isForgotPage ? store.gotoLogin() : store.gotoForgotPassword()">
+                        {{ store.isForgotPage ? 'Back to Login' : 'Forgot password?' }}
+                    </a>
                 </div>
             </form>
-            <UButton secondary class="w100 mt-6">
+            <UButton secondary class="w100 mt-6" v-if="!store.isForgotPage">
                 <div class="d-flex align-items-center justify-content-between gap-4">
                     <GoogleIcon style="font-size: 1.25em"/>
                     <span>Continue with Google</span>
                 </div>
             </UButton>
         </div>
-        <UButton flat secondary class="w100 mt-4 text-center register-btn" href="" @click.prevent="">
-            Dont have an account yet? Sign up
+        <UButton
+            flat secondary
+            class="w100 mt-4 text-center register-btn"
+            @click.prevent="() => store.isRegisterPage ? store.gotoLogin() : store.gotoRegister()">
+            {{ store.isRegisterPage ? 'Back to Login' : 'Dont have an account yet? Sign up' }}
         </UButton>
     </UModal>
 </template>
