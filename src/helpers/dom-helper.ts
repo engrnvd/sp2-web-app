bc3f402cd927b56b8eca02684e9da33c2a3306d3
Naveed Hassan <08es34@gmail.com>
2022-10-11 14:24:47 +0500

    * fixed dom helper

--- src/helpers/dom-helper.ts
+++ src/helpers/dom-helper.ts
@@ -2,5 +2,5 @@ import { _textWidth } from 'src/helpers/string-helper'
 
 export function _getInputTextSize(el: HTMLInputElement) {
   let styles = getComputedStyle(el)
-  return _textWidth(el.value.toString(), `${styles.fontSize} ${styles.fontFamily}`)
+  return _textWidth(el.value.toString(), `${styles.fontWeight} ${styles.fontSize} ${styles.fontFamily}`)
 }
