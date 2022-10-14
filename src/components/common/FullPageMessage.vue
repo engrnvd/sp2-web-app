2e9ef09a61743fa89fae800bf362d5f57d830845
Naveed Hassan <08es34@gmail.com>
2022-10-14 12:00:56 +0500

    * typed user

---
+++ src/components/common/FullPageMessage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="card p-4 text-muted all-center flex-column">
+        <div style="font-size: 14rem; color: rgba(0,0,0,0.1)">
+            <slot name="icon"></slot>
+        </div>
+        <div class="mb-5">
+            <slot></slot>
+        </div>
+        <slot name="action"></slot>
+    </div>
+</template>
